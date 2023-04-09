10d614204f807c0f8fd955adf28f1d191646f1a3
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-09 21:13:46 +0900

    게시물 리스트 구현

---
+++ src/main/java/com/KoreaIT/java/jam/ArticleListServlet.java
@@ -0,0 +1,67 @@
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.KoreaIT.java.jam.util.DBUtil;
+import com.KoreaIT.java.jam.util.SecSql;
+
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		
+		Connection conn = null;
+		String url = "jdbc:mysql://127.0.0.1:3306/JSPAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		}
+
+		try {
+			conn = DriverManager.getConnection(url, "root", "");
+			response.getWriter().append("Success!!");
+			
+			SecSql sql = new SecSql();
+			
+			sql.append("SELECT * FROM article");
+			sql.append("ORDER BY id DESC");
+			
+			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+			
+			response.getWriter().append(articleRows.toString());
+			
+			request.setAttribute("articleRows", articleRows);
+//			 서블릿에서 jsp에 뭔가를 알려줘야할때
+			
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
+			
+					
+		} catch (SQLException e) {
+			System.out.println("에러 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
